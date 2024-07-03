6be27bcc7843b737523e5361a3277052a6a4d9bd
lepridev <lepridev35@gmail.com>
2024-07-03 22:01:06 +0000

    Ajout de la logique d'update4

--- src/app/enregistrement/update/[id]/page.tsx
+++ src/app/enregistrement/update/[id]/page.tsx
@@ -3,12 +3,34 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
-const CreateEnregistrement = (props: Props) => {
+const UpdateEnregistrement = ({ params }: any) => {
+  const { id } = params;
   const router = useRouter();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getAllData = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/search`
+      );
+      if (res) {
+        setData(res.data?.data);
+        console.log(res.data?.data);
+        console.log("All Data fetch...");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const [enregistrement, setEnregistrement] = useState({
     reference: "",
     name: "",
@@ -31,41 +53,104 @@ const CreateEnregistrement = (props: Props) => {
     },
   });
 
+  const [distantData, setDistantData] = useState({
+    reference: "",
+    name: "",
+    lastname: "",
+    profession: "",
+    adresse: "",
+    contact: "",
+    lotissement: "",
+    ilot: 0,
+    lot: 0,
+    category: "",
+    superficie: 0,
+    observation: "",
+    file: "",
+    isFirstBuyer: "",
+    antecedant: [
+      {
+        vendeur: "",
+        contactvendeur: "",
+        adresseVendeur: "",
+      },
+    ],
+  });
+
   const handleChange = (e: any) => {
     const { name, value } = e.target;
-    setEnregistrement((prevState) => ({
+    setDistantData((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
   const handleAntecedantChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setEnregistrement((prevState) => ({
+    setDistantData((prevState) => ({
       ...prevState,
-      antecedant: {
-        ...prevState.antecedant,
-        [name]: value,
-      },
+      antecedant: [
+        {
+          ...prevState.antecedant[0],
+          [name]: value,
+        },
+      ],
     }));
   };
-
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     console.log(enregistrement);
+    let dataTosend = {
+      ...distantData,
+      // reference: distantData.reference,
+      // category: distantData.category,
+      // ilot: distantData.ilot,
+      // lot: distantData.lot,
+      // lotissement: distantData.lotissement,
+      // superficie: distantData.superficie,
+      // antecedant: {
+      //   vendeur: distantData.name,
+      //   contactvendeur: distantData.contact,
+      //   adresseVendeur: distantData.adresse,
+      // },
+      id: id,
+    };
+    console.log("dataTosend", dataTosend);
+
     try {
-      const res = await axios.post("/api/add/create", enregistrement);
-      if (res.data?.success === true) {
+      const res = await axios.post("/api/add/modifier", dataTosend);
+      if (res) {
+        getAllData();
         router.push("/recaps");
+        console.log("res", res);
+      }
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  const getDataByID = async () => {
+    try {
+      const res = await axios.post("/api/search/id", { id: id });
+      if (res.data) {
+        console.log("res.data.data", res.data.data);
+        setDistantData(res.data.data);
       }
     } catch (error) {}
   };
 
+  // console.log("dataTosend", dataTosend);
+
+  useEffect(() => {
+    getDataByID();
+  }, [id]);
+
+  console.log("distantData", distantData?.antecedant[0]);
   return (
     <div>
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
           <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
-            Enregistrer une nouvelle Attestation
+            Modifier l&apos;enregistrement
           </h2>
           <form onSubmit={handleSubmit}>
             <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
@@ -80,9 +165,9 @@ const CreateEnregistrement = (props: Props) => {
                   type="text"
                   name="reference"
                   id="reference"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="N° Reference"
-                  value={enregistrement.reference}
+                  value={distantData?.reference}
                   required
                   onChange={handleChange}
                 />
@@ -102,7 +187,7 @@ const CreateEnregistrement = (props: Props) => {
                   placeholder="Nom"
                   required
                   onChange={handleChange}
-                  value={enregistrement.name}
+                  value={distantData?.name}
                 />
               </div>
               <div className="w-full">
@@ -120,7 +205,7 @@ const CreateEnregistrement = (props: Props) => {
                   placeholder="Prénoms"
                   required
                   onChange={handleChange}
-                  value={enregistrement.lastname}
+                  value={distantData?.lastname}
                 />
               </div>
               <div className="w-full">
@@ -138,7 +223,7 @@ const CreateEnregistrement = (props: Props) => {
                   placeholder="Profession"
                   required
                   onChange={handleChange}
-                  value={enregistrement.profession}
+                  value={distantData?.profession}
                 />
               </div>
               <div className="w-full">
@@ -156,7 +241,7 @@ const CreateEnregistrement = (props: Props) => {
                   placeholder="Adresse"
                   required
                   onChange={handleChange}
-                  value={enregistrement.adresse}
+                  value={distantData?.adresse}
                 />
               </div>
               <div className="w-full">
@@ -174,7 +259,7 @@ const CreateEnregistrement = (props: Props) => {
                   placeholder="Contacts"
                   required
                   onChange={handleChange}
-                  value={enregistrement.contact}
+                  value={distantData?.contact}
                 />
               </div>
               <div className="w-full">
@@ -188,11 +273,11 @@ const CreateEnregistrement = (props: Props) => {
                   type="text"
                   name="lotissement"
                   id="lotissement"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Nom du lottissement"
                   required
                   onChange={handleChange}
-                  value={enregistrement.lotissement}
+                  value={distantData?.lotissement}
                 />
               </div>
               <div className="w-full">
@@ -206,11 +291,11 @@ const CreateEnregistrement = (props: Props) => {
                   type="text"
                   name="ilot"
                   id="ilot"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Ilot"
                   required
                   onChange={handleChange}
-                  value={enregistrement.ilot}
+                  value={distantData?.ilot}
                 />
               </div>
               <div className="w-full">
@@ -224,11 +309,11 @@ const CreateEnregistrement = (props: Props) => {
                   type="text"
                   name="lot"
                   id="lot"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="N° Lot"
                   required
                   onChange={handleChange}
-                  value={enregistrement.lot}
+                  value={distantData?.lot}
                 />
               </div>
               <div>
@@ -240,13 +325,15 @@ const CreateEnregistrement = (props: Props) => {
                 </label>
                 <select
                   onChange={handleChange}
-                  value={enregistrement.category}
+                  value={distantData?.category}
                   id="category"
                   name="category"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 >
                   <option>Choisisez la category</option>
-                  <option value="ACD">ACD</option>
+                  <option value="ACD" selected>
+                    ACD
+                  </option>
                   <option value="ATT">ATT Villageoise</option>
                 </select>
               </div>
@@ -261,11 +348,11 @@ const CreateEnregistrement = (props: Props) => {
                   type="number"
                   name="superficie"
                   id="superficie"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="400"
                   required
                   onChange={handleChange}
-                  value={enregistrement.superficie}
+                  value={distantData?.superficie}
                 />
               </div>
               <div className="sm:col-span-2">
@@ -281,7 +368,7 @@ const CreateEnregistrement = (props: Props) => {
                   rows={8}
                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Laisser un commentaire"
-                  value={enregistrement.observation}
+                  value={distantData?.observation}
                   onChange={handleChange}
                 ></textarea>
               </div>
@@ -296,7 +383,7 @@ const CreateEnregistrement = (props: Props) => {
                   id="file"
                   name="file"
                   type="file"
-                  value={enregistrement.file}
+                  value={distantData?.file}
                   onChange={handleChange}
                   className="block p-2.5 w-full text-sm text-gray-900 px-5 py-10 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 ></input>
@@ -309,7 +396,7 @@ const CreateEnregistrement = (props: Props) => {
                   Premier Acheteur
                 </label>
                 <select
-                  value={enregistrement.isFirstBuyer}
+                  value={distantData?.isFirstBuyer}
                   onChange={handleChange}
                   id="isFirstBuyer"
                   name="isFirstBuyer"
@@ -317,7 +404,9 @@ const CreateEnregistrement = (props: Props) => {
                 >
                   <option>Choisisez</option>
                   <option value="Oui">Oui</option>
-                  <option value="Non">Non</option>
+                  <option value="Non" selected>
+                    Non
+                  </option>
                 </select>
               </div>
               <h2>ANTECEDANT</h2>
@@ -332,11 +421,11 @@ const CreateEnregistrement = (props: Props) => {
                   type="text"
                   name="vendeur"
                   id="vendeur"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" @border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Nom du vendeur"
                   required
                   onChange={handleAntecedantChange}
-                  value={enregistrement.antecedant.vendeur}
+                  value={distantData?.antecedant[0].vendeur}
                 />
               </div>
               <div className="w-full">
@@ -350,11 +439,11 @@ const CreateEnregistrement = (props: Props) => {
                   type="text"
                   name="contactvendeur"
                   id="contactVendeur"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Contact du vendeur"
                   required
                   onChange={handleAntecedantChange}
-                  value={enregistrement.antecedant.contactvendeur}
+                  value={distantData?.antecedant[0].contactvendeur}
                 />
               </div>
               <div className="w-full">
@@ -368,11 +457,11 @@ const CreateEnregistrement = (props: Props) => {
                   type="text"
                   name="adresseVendeur"
                   id="adresseVendeur"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Adresse du vendeur"
                   required
                   onChange={handleAntecedantChange}
-                  value={enregistrement.antecedant.adresseVendeur}
+                  value={distantData?.antecedant[0]?.adresseVendeur}
                 />
               </div>
             </div>
@@ -381,14 +470,14 @@ const CreateEnregistrement = (props: Props) => {
                 type="submit"
                 className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-green-600 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
               >
-                Creer l&apos;enregistrement
+                Modifier l&apos;enregistrement
               </button>
               <Link href={"/guide"}>
                 <button
                   type="submit"
                   className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-red-600 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
                 >
-                  Annuler / Retour
+                  Annuler
                 </button>
               </Link>
             </div>
@@ -399,4 +488,4 @@ const CreateEnregistrement = (props: Props) => {
   );
 };
 
-export default CreateEnregistrement;
+export default UpdateEnregistrement;
